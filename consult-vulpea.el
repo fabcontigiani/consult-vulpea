9a8423390c94eca5583ef81bf0582c7161f63563
Fabrizio Contigiani <fabcontigiani@gmail.com>
2026-09-06 17:53:28 -0300

    Bump version

--- consult-vulpea.el
+++ consult-vulpea.el
@@ -5,7 +5,7 @@
 ;; Author: Fabrizio Contigiani <fabcontigiani@gmail.com>
 ;; Maintainer: Fabrizio Contigiani <fabcontigiani@gmail.com>
 ;; URL: https://github.com/fabcontigiani/consult-vulpea
-;; Version: 0.3.0
+;; Version: 0.3.1
 ;; Package-Requires: ((emacs "28.1") (vulpea "2.0.0") (consult "2.2"))
 ;; Keywords: convenience, notes, vulpea
 
